fe47e74350a547a166ffe94f39aafcab7ae22eb6
dimas merida <dimas@olahagency.com>
2021-06-17 18:26:17 -0400

    Institutes ok

---
+++ src/components/admin/courses/list.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="list">
+    <h1>list Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'list',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .list {
+
+  }
+</style>
